fd22bc88995e70e31a44ffb47ecc89023bee130a
Iris <irisale.ayala@gmail.com>
2024-03-03 18:53:53 -0600

    modificaciones leves por tiempos de procesos

--- HTD5-EventSimulation_Simpy.py
+++ HTD5-EventSimulation_Simpy.py
@@ -4,9 +4,9 @@ import csv
 
 # Parámetros de la simulación
 intervalo = 10
-num_procesos = 25
+num_procesos = 200
 num_instrucciones = 3
-random.seed(11)
+random.seed(44)
 lista_procesos = []
 
 class Proceso:
@@ -18,7 +18,7 @@ class Proceso:
         self.instrucciones = random.randint(1, 10)
         self.memoria = random.randint(1, 10)
         self.hora_inicio = None
-        self.hora_fin = 0
+        self.hora_fin = None
 
     def ejecutar(self):
         print(f"{self.env.now} [NEW] {self.name}, RAM requerida -> {self.memoria}")
@@ -29,38 +29,38 @@ class Proceso:
             # Solicita CPU
             with self.cpu.request() as req:
                 yield req
-                if self.hora_inicio == None:
+                if self.hora_inicio is None:
                     self.hora_inicio = self.env.now
                 # Ejecuta sus instrucciones
-                print(f"{self.env.now} [RUNNING] {self.name}, ejecutando hasta {num_instrucciones} instrucciones.")
+                print(f"{self.env.now} [RUNNING] {self.name}, hasta {num_instrucciones} instrucciones.")
                 # Espera unidad de tiempo procesador
                 yield self.env.timeout(1)
                 self.instrucciones -= num_instrucciones  # Reducir las instrucciones restantes
                 # Pasa a IO o termina
                 if self.instrucciones <= 0:
-                    # Tiempo de fin
-                    self.hora_fin = self.env.now
-                    print(f"{self.env.now} [TERMINATED] {self.name} terminado.")
-                    cpu.release(req)
                     break
                 else:
                     random_num = random.randint(1, 2)
                     if random_num == 1:
                         # Espera en IO
                         print(f"{self.env.now} [WAITING] {self.name} -> Haciendo operaciones de I/O.")     
-                    # Regresa a la cola de CPU esperando tiempo de llegada de proceso
-                    yield env.timeout(round(random.expovariate(1/intervalo)), 1)
-                    print(f"{self.env.now} [READY] {self.name} -> instrucciones pendientes {self.instrucciones}.")
-                    yield req
+                        yield env.timeout(round(random.expovariate(1.0/intervalo)),1)
+                    else:
+                        # Regresa a la cola de CPU esperando tiempo de llegada de proceso
+                        print(f"{self.env.now} [READY] {self.name} -> instrucciones pendientes {self.instrucciones}.")
         # Retorna la memoria utilizada.
-        yield self.ram.put(self.memoria)
+        self.ram.put(self.memoria)
+        # Tiempo de fin
+        self.hora_fin = self.env.now
+        print(f"{self.env.now} [TERMINATED] {self.name} terminado.")
+        #cpu.release(req)
 
 def crear_proceso(env, ram, cpu, num_procesos):
     for i in range(num_procesos):
         proceso = Proceso(f"Proceso {i}", env, ram, cpu)
-        env.process(proceso.ejecutar())
         lista_procesos.append(proceso)
-        
+        env.process(proceso.ejecutar())
+                      
 # Configuración de la simulación
 env = simpy.Environment()
 ram = simpy.Container(env, init=100, capacity=100)
@@ -76,12 +76,12 @@ with open(nombre_archivo, mode='w', newline='') as file:
     writer = csv.writer(file)
 
     # Escribir encabezados
-    writer.writerow(['Proceso', 'Hora de inicio', 'Hora de fin', 'Duración'])
+    writer.writerow([f'{num_procesos}',f'Proceso #{num_procesos}', 'Hora de inicio', 'Hora de fin', 'Duración'])
 
     # Iterar sobre los procesos y escribir los datos
     for proceso in lista_procesos:
         duracion = proceso.hora_fin - proceso.hora_inicio
-        writer.writerow([proceso.name, proceso.hora_inicio, proceso.hora_fin, duracion])
+        writer.writerow([num_procesos, proceso.name, proceso.hora_inicio, proceso.hora_fin, duracion])
 
 print(f"Los datos se han exportado en '{nombre_archivo}'")
 
